258943334031abb04872744bb0e26333060cb637
adeaver <deaver.andrew@gmail.com>
2015-11-11 10:57:56 -0200

    Fixed minor bugs

--- Code/fb_analyze.py
+++ Code/fb_analyze.py
@@ -1,20 +1,22 @@
 import facebook, sys
 from pattern.en import sentiment
 
-token = "your_access_token_here"
+token = "your_access_token"
 graph = facebook.GraphAPI(access_token=token)
 
 post_data = graph.get_connections(id="some_id_here", connection_name="posts")["data"]
 
-index = sys.argv[0] if len(sys.argv) > 0 else 0
-index = index
+index = int(sys.argv[1]) if len(sys.argv) > 1 else 0
+index = index % len(post_data)
 
-while "message" not in post_data[index % len(post_data)]:
+while "message" not in post_data[index]:
     index += 1
+    index = index % len(post_data)
 
 status = post_data[index % len(post_data)]["message"]
 sent = sentiment(status)[0]
+print status
 
 f = open("sentiment.txt", "w")
-f.write(sent)
+f.write(str(sent))
 f.close()
